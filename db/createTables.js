125230d5b32b9e99e2ef9d5b08c784c5533d442d
Carlos Hernandez <124746055+carloshernandez23@users.noreply.github.com>
2023-04-25 16:31:48 -0400

    Update createTables.js

--- db/createTables.js
+++ db/createTables.js
@@ -1,9 +1,11 @@
 const db = require('./db');
 
 const createUsersTable = () => {
+  db.prepare('DROP TABLE IF EXISTS users').run();
   const statement = db.prepare(`
     CREATE TABLE IF NOT EXISTS users (
     user_id TEXT PRIMARY KEY,
+    created_at DATE NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL    
@@ -13,9 +15,27 @@ const createUsersTable = () => {
   statement.run();
 };
 
+const createNotesTable = () => {
+  db.prepare('DROP TABLE IF EXISTS notes').run();
+  const statement = db.prepare(`
+    CREATE TABLE IF NOT EXISTS notes (
+    note_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at DATE NOT NULL,
+    content TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    FOREIGN KEY (user_id)
+     REFERENCES users (user_id)
+      ON DELETE CASCADE   
+    )
+    `);
+
+  statement.run();
+};
+
 const createTables = async () => {
   console.log('Creando tablas...');
-  await createUsersTable();
+  createUsersTable();
+  createNotesTable();
   console.log('Tablas creadas!');
 };
 
